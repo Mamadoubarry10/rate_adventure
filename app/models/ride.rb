a9c94e236fb926714cbb567d0d9be15085cdb9f5
Mamadoubarry10 <Mamadou.barry1105@gmail.com>
2020-10-01 21:20:33 -0400

    near complete

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,4 +6,9 @@ class Ride < ApplicationRecord
     has_many :users, through: :message_boards
 
     acts_as_commontable
+
+    def avg_rating
+        self.message_boards.distinct.average(:rating).round()
+    end
+    
 end
